b1bce43dc692255b442b26c85bb1728038228b91
Shivraj Rath <shivrajrath@gmail.com>
2015-01-13 22:40:41 -0800

    Adds temporary fix for the scroll issue

--- app/js/global.js
+++ app/js/global.js
@@ -2,3 +2,18 @@ $(function () {
     'use strict';
     $('[data-toggle="tooltip"]').tooltip();
 });
+
+//Fix for scrollbar on all section
+$(window).on("orientationchange, resize", function () {
+    'use strict';
+    adjustMainScroll();
+});
+
+$(document).ready(function () {
+    'use strict';
+    adjustMainScroll();
+});
+
+function adjustMainScroll() {
+    $(".navlink-section,.product-list-section,.main-section").outerHeight($(".mkit").outerHeight() - $(".mkit-head").outerHeight() + 15);
+}
